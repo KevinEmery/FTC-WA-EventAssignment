e820f7130fee27d4951dbc6399682b57199d775a
Kevin Emery <360emery@gmail.com>
2024-07-27 20:21:08 -0700

    Update distance calculations to use miles

--- util.py
+++ util.py
@@ -5,13 +5,19 @@ from uszipcode import SearchEngine
 from model.league import League
 from model.team import Team
 
+MILES_PER_KM = 0.621371
+
 def calculate_distance_between_team_and_league(team: Team, league: League):
     return _calc_distance_between_zip_codes(team.zip_code, league.zip_code)
 
+# Returns the distance in miles
 def _calc_distance_between_zip_codes(zip1: str, zip2: str) -> float:
     search = SearchEngine()
 
     zip1_coords = search.by_zipcode(zip1)
     zip2_coords = search.by_zipcode(zip2)
 
-    return mpu.haversine_distance((zip1_coords.lat,zip1_coords.lng),(zip2_coords.lat,zip2_coords.lng))
+    dist_km = mpu.haversine_distance((zip1_coords.lat,zip1_coords.lng),(zip2_coords.lat,zip2_coords.lng))
+    dist_miles = dist_km * MILES_PER_KM
+
+    return dist_miles
